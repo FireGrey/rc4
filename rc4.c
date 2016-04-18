de9ca5f1223908b38b3cdc21f6848eeec41011d8
David Maxwell <dmaxwell@Davids-MBP-3.gateway>
2016-04-18 15:41:18 +0800

    implemented key

--- rc4.c
+++ rc4.c
@@ -1,37 +1,51 @@
 #include <stdio.h>
+#include <stdint.h>
 
-int main(void) 
+int main(int argc, char *argv[])
 {
-		
+	// ----- Initialising Key -----
+
+	uint8_t key[16] = {0x09, 0x8F, 0x6B, 0xCD, 0x46, 0x21, 0xD3, 0x73, 0xCA, 0xDE, 0x4E, 0x83, 0x26, 0x27, 0xB4, 0xF6};
+
+	int keylength = 16;
+
+	// ----- Initialising Message
+
+	uint8_t message[41] = {0x8E, 0xBB, 0x63, 0x13, 0xE8, 0xEA, 0x01, 0xC2, 0xF0, 0xF3, 0x81, 0xE5, 0x07, 0x1C, 0xB6, 0x63, 0xB3, 0x7E, 0x26, 0x5A, 0x86, 0x25, 0x82, 0x98, 0x86, 0xEA, 0x19, 0x09, 0xE6, 0xB9, 0x5A, 0x22, 0x65, 0xB7, 0xB4, 0xF8, 0x61, 0x16, 0x9D, 0xC7, 0x4A};
+
+	int messagelength = 41;
 
 	// ----- Key Scheduling Algorithm -----
 	
 	// Initialising S
 	
-	for(i=0; i<256; i++)
+	uint8_t S[255];	
+
+	for(int i=0; i<256; i++)
 		S[i] = i;
 	
-	// 
-	
-	for(i=0, j=0; i<256; i++)
+	for(int i=0, j=0; i<256; i++)
 	{
 		j = (j + S[i] + key[i % keylength]) % 256;
-		int temp = S[i];
+		uint8_t temp = S[i];
 		S[i] = S[j];
 		S[j] = temp;
 	}
 	
 	// ----- Pseudo-Random Generation Algorithm -----
 	
-	int i = 0;
-	int j = 0;
-	
-	do
+	uint8_t K[messagelength];
+
+	for(int i=0, j=0; messagelength > 0; messagelength--)
 	{
 		i = (i + 1) % 256;
-		j = (j + S[i] % 256;
+		j = (j + S[i]) % 256;
 		int temp = S[i];
 		S[i] = S[j];
 		S[j] = temp;
-		int keystream = S[S[i] + S[j] % 256];
+		K[i] = S[S[i] + S[j] % 256];
+		// printf("[#%d: ", messagelength);
+		// printf("%u]", K[i]);
+		// printf("%x", K[i]^message[i]);
 	}
+}
